d9edee6cf2b9d356bfe42fe9d677d98523ae7324
cbsumastre <cbsumastre@gmail.com>
2020-08-01 13:36:28 +0200

    Pass data with props

---
+++ src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div v-if="childMessage">
+      With props: <span class="message">{{ childMessage }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    childMessage: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.message {
+  color: #18ab29;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <h1 class="title">Pass data between components</h1>
+    <textarea
+      class="parent"
+      v-model="parentMessage"
+      placeholder="Enter new message in parent"
+    ></textarea>
+    <button v-on:click="send" class="myButton">Send message to child</button>
+    <Child :childMessage="childMessage" class="child"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Child from "@/components/Child";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Child
+  },
+  data: function() {
+    return {
+      parentMessage: "",
+      childMessage: ""
+    };
+  },
+  methods: {
+    send() {
+      this.childMessage = this.parentMessage;
+      //clear parent message
+      this.parentMessage = "";
+    }
   }
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  margin: 20px 0;
+  min-height: 200px;
+}
+.title {
+  width: 50%;
+  color: #18ab29;
+}
+.parent {
+  width: 50%;
+  min-height: 150px;
+  background-color: orange;
+  margin-top: 25px;
+  margin-bottom: 25px;
+}
+.myButton {
+  box-shadow: 0px 0px 0px 0px #3dc21b;
+  background-color: #44c767;
+  border-radius: 10px;
+  border: 2px solid #18ab29;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 14px;
+  padding: 19px 31px;
+  text-decoration: none;
+}
+.myButton:hover {
+  background-color: #5cbf2a;
+}
+.myButton:active {
+  position: relative;
+  top: 1px;
+}
+
+.child {
+  width: 100%;
+  min-height: 150px;
+  margin-top: 25px;
+}
+</style>
